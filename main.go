da5b1da392a0993011db36ab9d4e80eefdc43bbf
Chainat Lumyos <basbas2812@gmail.com>
2026-07-14 11:39:24 +0700

    add middleware

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"john":  "doe",
+			"admin": "123456",
+		},
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!!!")
